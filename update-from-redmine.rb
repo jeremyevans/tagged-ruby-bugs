d490e5c5d36dba62d208bc377fd4ed76c7039c2c
Jeremy Evans <code@jeremyevans.net>
2022-06-10 09:18:40 -0700

    Add/remove feedback tag automatically based on bug status
    This will allow easily filtering for feedback status in the user
    interface.

--- update-from-redmine.rb
+++ update-from-redmine.rb
@@ -34,14 +34,28 @@ existing_rows = CSV.read(BUG_FILE, encoding: 'UTF-8').map{|x| Bug.create(*x)}
 
 body = Net::HTTP.get(URI('https://bugs.ruby-lang.org/projects/ruby-master/issues?c%5B%5D=tracker&c%5B%5D=status&c%5B%5D=subject&c%5B%5D=assigned_to&c%5B%5D=updated_on&f%5B%5D=status_id&f%5B%5D=tracker_id&f%5B%5D=&group_by=&op%5Bstatus_id%5D=o&op%5Btracker_id%5D=%3D&per_page=500&set_filter=1&sort=id%3Adesc&utf8=.&v%5Btracker_id%5D%5B%5D=1'))
 
-body.scan(%r{<td class="subject"><a href="/issues/(\d+)">([^<]+)</a></td>}).each do |id, subject|
-  if bug = Bug::MAP[id]
-    bug.subject = subject
-    bug.active = true
+last_status = nil
+body.scan(%r{<td class="subject"><a href="/issues/(\d+)">([^<]+)</a></td>|<td class="status">([^<]+)</td>}).each do |id, subject, status|
+  if id
+    last_id = id
+    if bug = Bug::MAP[id]
+      bug.subject = subject
+      bug.active = true
+    else
+      puts "New Ruby Bug: #{id}"
+      bug = Bug.create(id, '', subject)
+      bug.active = true
+    end
+
+    tags = bug.tags.split
+    if last_status == 'Feedback'
+      tags << 'feedback'
+    else
+      tags.delete('feedback')
+    end
+    bug.tags = tags.uniq.join(' ')
   else
-    puts "New Ruby Bug: #{id}"
-    bug = Bug.create(id, '', subject)
-    bug.active = true
+    last_status = status
   end
 end
 
